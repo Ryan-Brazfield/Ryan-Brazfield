246a1ed2967430497c3421de230cd1b08897c48a
ryan-brazfield <ryan.brazfield@gmail.com>
2021-12-31 12:23:18 -1000

    gitinit

---
+++ script.js
@@ -0,0 +1,18 @@
+var message="Hello Bitches"
+var responses="Aloha Beaches"
+var number=2
+var string="2"
+var total=times4(number)
+function sayhello(){
+    alert(message)
+    return message
+}
+function times4(x){
+    return x*4
+}
+
+function alohabeaches(){
+    alert (responses)
+    return responses
+
+}
